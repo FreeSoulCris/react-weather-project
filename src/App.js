f405bcb03f06f2673729b8e7b29168afe6da0898
FreeSoulCris <c.jimenez@witbooking.com>
2024-05-20 13:39:01 +0200

    I added footer links

--- src/App.js
+++ src/App.js
@@ -3,4 +3,12 @@ import React from 'react';
 import './App.css';
 
  export default function App (){
-  return <div className="App"> Weather App </div>;}
+  return <div className="App"> 
+ <h1>Weather App</h1> 
+  <footer>
+   This project was coded by <a href="https://www.instagram.com/freesoulcriss/" target="_blank"> Cristina Jimenez</a>, is open-sourced on 
+  <a href="https://github.com/FreeSoulCris/react-weather-project" target="_blank"> Github </a> and hosted on <a href="https://weather-application-project-react.netlify.app/" target="_blank">Netlify</a>
+  </footer>
+   
+  </div>;
+  }
