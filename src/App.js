e2e66b8d12c6581938f0a387bca1097c1267ba47
FreeSoulCris <c.jimenez@witbooking.com>
2024-05-20 19:44:12 +0200

    i added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import React from 'react';
 import './App.css';
 
  export default function App (){
-  return <div className="App"> 
- <h1>Weather App</h1> 
-  <footer>
-   This project was coded by {" "}<a href="https://www.instagram.com/freesoulcriss/" target="_blank" rel="noreferrer">Cristina Jimenez</a>, is open-sourced on {" "}
-  <a href="https://github.com/FreeSoulCris/react-weather-project" target="_blank" rel="noreferrer">Github </a> and hosted on {" "}<a href="https://weather-application-project-react.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
-  </footer>
-   
-  </div>;
+  return ( 
+  <div className="App"> 
+    <div className="container">
+      <h1>Weather App</h1> 
+       <footer>
+          This project was coded by {" "}<a href="https://www.instagram.com/freesoulcriss/" target="_blank" rel="noreferrer">Cristina Jimenez</a>, is open-sourced on {" "}
+          <a href="https://github.com/FreeSoulCris/react-weather-project" target="_blank" rel="noreferrer">Github </a> and hosted on {" "}<a href="https://weather-application-project-react.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
+       </footer>
+    </div>
+  </div>
+  );
   }
