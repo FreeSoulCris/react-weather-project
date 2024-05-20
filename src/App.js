3c9aa66e37330420e1ffab5fd07f987ccfb0042b
FreeSoulCris <c.jimenez@witbooking.com>
2024-05-20 13:41:52 +0200

    I added rel="noreferrer"

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
   return <div className="App"> 
  <h1>Weather App</h1> 
   <footer>
-   This project was coded by {" "}<a href="https://www.instagram.com/freesoulcriss/" target="_blank">Cristina Jimenez</a>, is open-sourced on {" "}
-  <a href="https://github.com/FreeSoulCris/react-weather-project" target="_blank">Github </a> and hosted on {" "}<a href="https://weather-application-project-react.netlify.app/" target="_blank">Netlify</a>
+   This project was coded by {" "}<a href="https://www.instagram.com/freesoulcriss/" target="_blank" rel="noreferrer">Cristina Jimenez</a>, is open-sourced on {" "}
+  <a href="https://github.com/FreeSoulCris/react-weather-project" target="_blank" rel="noreferrer">Github </a> and hosted on {" "}<a href="https://weather-application-project-react.netlify.app/" target="_blank" rel="noreferrer">Netlify</a>
   </footer>
    
   </div>;
