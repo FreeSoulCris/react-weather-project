a80d46e1c669ff50ae15d8279a93944af9200bcf
FreeSoulCris <c.jimenez@witbooking.com>
2024-06-09 16:12:24 +0200

    API calls all set

--- src/Weather.js
+++ src/Weather.js
@@ -1,57 +1,84 @@
-import React from "react"; 
+import React, { useState } from "react"; 
+import axios from "axios" ;
 import "./Weather.css";
 
-export default function Weather() {
-    return (<div className="Weather">
-     < form>
-     <div className="row"> 
-        <div className="col-9">
-        <input type="search" placeholder="Enter a city .." className="form-control"
-        autoFocus="On"/>
-        </div>
-        <div className="col-3">
-        <input type="submit" value="Search" className="btn btn-primary w-100"/>
-        </div>
-        </div>
-     </form>
-        <h1>New York</h1>
-        <ul>
-            <li>
-                Wednesday 07:00
-            </li>
-            <li>
-                Mostly Cloudy
-            </li>
-        </ul>
-        <div className="row">
-            <div className="col-6">
-                <div className="weather-info">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt="partly cloudy"
-                className="float-left"
-                /> 
-            <div className="float-left">
-               <span className="temperature">6</span> 
-               <span className="unit">ºC</span>
-               </div>
-                </div>
-            </div>
-            <div className="col-6">
-            <ul>
-            <li>
-                Precipitation: 15%
-            </li>
-            <li>
-                Humidity:72%
-            </li>
-            <li>
-                Wind: 13 km/h
-            </li>
-        </ul>
-            </div>
-        </div>
 
-    </div>
-);
+
+export default function Weather() {
+    const [weatherData, setWeatherData] = useState ({ready : false});
+    function handleResponse(response) {
+        console.log(response.data);
+
+        setWeatherData({
+            ready:true,
+            temperature: response.data.temperature.current,
+            wind: response.data.wind.speed,
+            humidity: response.data.temperature.humidity,
+            city: response.data.city,
+            description :response.data.condition.description,
+            iconUrl: response.data.condition.icon_url,
+            date: "Sunday 16:00"
+        })
+
+    }
+
+    if (weatherData.ready){
+        return (<div className="Weather">
+            < form>
+            <div className="row"> 
+               <div className="col-9">
+               <input type="search" placeholder="Enter a city .." className="form-control"
+               autoFocus="On"/>
+               </div>
+               <div className="col-3">
+               <input type="submit" value="Search" className="btn btn-primary w-100"/>
+               </div>
+               </div>
+            </form>
+               <h1>{weatherData.city}</h1>
+               <ul>
+                   <li>
+                       {weatherData.date}
+                   </li>
+                   <li className="text-capitalize">
+                       {weatherData.description}
+                   </li>
+               </ul>
+               <div className="row">
+                   <div className="col-6">
+                       <div className="weather-info">
+                     <img
+                       src={weatherData.iconUrl}
+                       alt={weatherData.description}
+                       className="float-left"
+                       /> 
+                   <div className="float-left">
+                      <span className="temperature">{Math.round(weatherData.temperature)}</span> 
+                      <span className="unit">ºC</span>
+                      </div>
+                       </div>
+                   </div>
+                   <div className="col-6">
+                   <ul>
+                   <li>
+                       Humidity:{Math.round(weatherData.humidity)}%
+                   </li>
+                   <li>
+                       Wind: {Math.round(weatherData.wind)} km/h
+                   </li>
+               </ul>
+                   </div>
+               </div>
+       
+           </div>
+       );
+
+    } else {
+    const apiKey ="0239330ab540e803o5b4f9t7e63fbef4";
+    let city = "Barcelona";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
+    
+    return "Loading..."
+}
 }
