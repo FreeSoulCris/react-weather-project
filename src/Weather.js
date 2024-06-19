2c11bd8f15c2073e7b1b5b7403834b6ca4a0f565
FreeSoulCris <c.jimenez@witbooking.com>
2024-06-19 23:48:18 +0200

    Form logic done

--- src/Weather.js
+++ src/Weather.js
@@ -8,6 +8,7 @@ import WeatherInfo from "./WeatherInfo";
 
 export default function Weather(props) {
     const [weatherData, setWeatherData] = useState ({ready : false});
+    const [city, setCity] = useState(props.defaultCity);
     function handleResponse(response) {
         console.log(response.data);
 
@@ -23,14 +24,32 @@ export default function Weather(props) {
         })
 
     }
+    function search(){
+        //city
+        const apiKey ="0239330ab540e803o5b4f9t7e63fbef4";
+         let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+        axios.get(apiUrl).then(handleResponse);
+    }
+
+
+    function handleSubmit (event) {
+        event.preventDefault();
+        //Search for a city
+        search();
+    }
+
+    function handleCityChange (event){
+        setCity(event.target.value);
+    }
+
 
     if (weatherData.ready){
         return (
         <div className="Weather">
-            < form>
+            < form onSubmit={handleSubmit}>
                 <div className="row"> 
                     <div className="col-9">
-                        <input type="search" placeholder="Enter a city .." className="form-control" autoFocus="On"/>
+                        <input type="search" placeholder="Enter a city .." className="form-control" autoFocus="On" onChange={handleCityChange}/>
                     </div>
                     <div className="col-3">
                         <input type="submit" value="Search" className="btn btn-primary w-100"/>
@@ -42,10 +61,7 @@ export default function Weather(props) {
        );
 
     } else {
-    const apiKey ="0239330ab540e803o5b4f9t7e63fbef4";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${props.defaultCity}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
-    
+    search();  
     return "Loading..."
 }
 }
